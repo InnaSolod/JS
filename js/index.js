557b5fc21b39ff127f4415f02806cad58684d21f
InnaSolod <solodinna6@gmail.com>
2022-08-05 15:00:34 +0300

    save

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
-function checkForSpam(message) {
-    let result;
-    // Change code below this line
-    const normalizedInput = message.toLowerCase ();
-    console.log(normalizedInput)
-    result = normalizedInput.includes('spam') || normalizedInput.includes('sale')
-    // Change code above this line
-    return result;
-  }
-  console.log(checkForSpam('Get best sale offers now!'))
+// function checkForSpam(message) {
+//     let result;
+//     // Change code below this line
+//     const normalizedInput = message.toLowerCase ();
+//     console.log(normalizedInput)
+//     result = normalizedInput.includes('spam') || normalizedInput.includes('sale')
+//     // Change code above this line
+//     return result;
+//   }
+//   console.log(checkForSpam('Get best sale offers now!'))
+
+// Массивы
+
+// const friends = ['Mango', 'Kiwi', 'Poly', 'Ajax'];
+// const lastIndex = friends.length - 1;
+
+// for(let i = 0; i <= lastIndex; i +=1) {
+//   console.log(friends[i]);
+// }
+
+// for (const friend of friends) {
+//   console.log(friend);
+// }
+
