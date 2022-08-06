5f70fd0c52880720554344626b3fbca30420d6b8
InnaSolod <solodinna6@gmail.com>
2022-08-06 21:04:24 +0300

    save

--- js/02-payment.js
+++ js/02-payment.js
@@ -1,3 +1,16 @@
 // Посчитать общую сумму покупок в корзине
 
-const cart = [54, 28, 105, 78, 92, 17, 120];
+const cart = [54, 28, 105, 70, 92, 17, 120, 12, 25, 90];
+// console.log(cart.length);
+let total = 0;
+
+// for (let i = 0; i < cart.length; i += 1) {
+//     // console.log(cart[i]);
+//     total += cart[i];
+//     // total = total + cart[i];
+// }
+for (const value of cart) {
+    total += value;
+}
+
+console.log('Total: ', total);
