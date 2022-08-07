3d2dedea8e84d03188fc96d2728624e857dea23c
InnaSolod <solodinna6@gmail.com>
2022-08-07 11:00:17 +0300

    save

--- js/04-find-login.js
+++ js/04-find-login.js
@@ -1,2 +1,25 @@
 // Напиши скрипт поиска логина
 // - Если логина нет, вывести сообщение "Пользователь [логин] не найден."
+// - Если нашли логин, вывести сообщение "Пользователь [логин] найден."
+
+// - Сначала через for
+// - Потом через for...of
+// - Логика break
+// - Метод includes() с тернарным оператором
+
+const logins = ['m4ngoDoge', 'kiwidab3st', 'poly1scute', 'aj4xth3m4n'];
+const loginToFind = 'poly1scute';
+let message = '';
+
+for (let i = 0; i < logins.length; i += 1) {
+    const login = logins[i];
+
+    if (login === loginToFind) {
+        message = `Пользователь ${loginToFind} найден.`;
+        break;
+    }
+
+    message = `Пользователь ${loginToFind} не найден.`;
+}
+
+console.log(message);
