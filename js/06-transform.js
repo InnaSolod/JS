3ebd4db352e4775a35f56ae520d790d0aca9c8fb
InnaSolod <solodinna6@gmail.com>
2022-08-07 15:47:27 +0300

    save

---
+++ js/06-transform.js
@@ -0,0 +1,19 @@
+// Напиши скрипт, который обьединяет все элементы массива в одно строковое значение.
+// Элементов может быть произвольное кол-во.
+// - Пусть элементы массива в строке будут разделены запятой.
+// - Сначала через for
+// - Потом через join()
+
+const friends = ["Mango", "Poly", "Kiwi", "Ajax"];
+// let string = "";
+
+// for (const friend of friends) {
+//   string += friend + ",";
+// }
+// string = string.slice(0, string.length - 1);
+
+const string = friends.join(",");
+
+console.log(string);
+
+// Должно получится 'Mango,Poly,Kiwi,Ajax'
